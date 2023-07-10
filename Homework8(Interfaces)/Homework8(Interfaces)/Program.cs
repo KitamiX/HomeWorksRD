56ee3dece2b24d358205b938d0d61df449d396a3
KitamiX <Xwernon@ukr.net>
2023-07-10 23:50:04 +0300

    Homework8(Interfaces)
    Homework8(Interfaces)

---
+++ Homework8(Interfaces)/Homework8(Interfaces)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework8_Interfaces_
+{
+    internal class Program
+    {
+        public static void TestCar(Car car)
+        {
+            car.GetCarProp();
+        }
+        static void Main(string[] args)
+        {
+            TestCar(new Audi());
+            TestCar(new BMW());
+            Console.WriteLine("======================");
+            var audi = new Audi();
+            audi.SpeedUp();
+            audi.SpeedUp();
+            audi.SpeedUp();
+            audi.SpeedUp();
+            audi.SpeedUp();
+            audi.SlowDown();
+            audi.GetSpeed();
+            Console.WriteLine("=====================");
+            audi.HeaterOn();
+            audi.HeaterOn();
+            audi.HeaterOn();
+            audi.HeaterOn();
+            Console.WriteLine("=====================");
+            audi.HeaterOff();
+            audi.HeaterOff(); 
+            audi.HeaterOff();
+        }
+    }
+}
---
+++ Homework8(Interfaces)/Homework8(Interfaces)/Sevices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_Interfaces_
+{
+    public class Seats : ISeats
+    {
+        public void HeatOn(bool status)
+        {
+            if (status)
+                Console.WriteLine("Heater is already ON");
+            else
+                Console.WriteLine("Seat Heater is now ON!!!");
+        }
+        public void HeatOff(bool status)
+        {
+            if (!status)
+                Console.WriteLine("Heater is already OFF");
+            else
+                Console.WriteLine("Seat Heater is now OFF!!!");
+        }
+        public void AdjustPosition(int positionDegree) 
+        {
+            Console.WriteLine($"Seat position was changed to {positionDegree} degrees");
+        }
+    }
+
+    public class Radio : IRadio
+    {
+        public void TurnOn(bool status)
+        {
+            if (status)
+                Console.WriteLine("Radio is already ON");
+            else
+                Console.WriteLine("Radio is now ON!!!");
+        }
+        public void TurnOff(bool status)
+        {
+            if (!status)
+                Console.WriteLine("Radio is already OFF");
+            else
+                Console.WriteLine("Radio is now OFF!!!");
+        }
+        public void ChangeStation (int stationFreq) 
+        {
+            Console.WriteLine($"Now playing {stationFreq} Mhz Radio Station");
+        }
+    }
+}
