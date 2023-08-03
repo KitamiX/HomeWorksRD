fee3044889dd3ff6f4863672423a000f3268ff05
KitamiX <Xwernon@ukr.net>
2023-08-03 23:45:35 +0300

    Homework10(Strings)
    Homework10(Strings)

---
+++ Homework10(Strings)/Homework10(Strings)/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework10_Strings_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //work in progress...
+            //not yet done, will be tomorrow or till 23:59 08/08/23
+        }
+    }
+}
