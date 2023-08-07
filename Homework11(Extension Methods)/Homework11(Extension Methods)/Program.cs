89b72ba5dd3a72404d7fb8101d4188f7aa53da37
KitamiX <Xwernon@ukr.net>
2023-08-07 21:40:24 +0300

    Homework11
    Extension Methods

---
+++ Homework11(Extension Methods)/Homework11(Extension Methods)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework11_Extension_Methods_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //work in progress...
+        }
+    }
+}
